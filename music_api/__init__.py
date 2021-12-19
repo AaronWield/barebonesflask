7d8a27687f99a62eb4043637b28f712fdedd9a6f
Aaron Wieldraayer <90867179+AaronWield@users.noreply.github.com>
2021-12-19 12:54:29 -0500

    2

--- music_api/__init__.py
+++ music_api/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from .authentication.routes import auth
 from .site.routes import site
+from .models import db, User
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 app.register_blueprint(auth)
 app.register_blueprint(site)
--- music_api/authentication/routes.py
+++ music_api/authentication/routes.py
@@ -1,12 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from music_api.forms import UserLogInForm
+from music_api.models import db, User
+
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = UserLogInForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print(email,password)
+        new_user = User(email, password)
+        db.session.add(new_user)
+        db.session.commit()
+    return render_template('signup.html', form = form)
 
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    form = UserLogInForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        print(email,password)
+    return render_template('signin.html', form = form)
